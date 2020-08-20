47316185d7cdfbc8eb0f17a4fcd7da83c9d85486
linLlee <109227225+linLlee@users.noreply.github.com>
2020-08-20 20:02:45 +0000

    Remove config file

--- spiders/spiders/awsdocs.py
+++ spiders/spiders/awsdocs.py
@@ -161,3 +161,50 @@ class AwsDocsSpider(Spider):
     resource_options = [
         '--instance-ids',
         '--bucket',
+        '--cluster-states',
+    ]
+    base_url = 'http://docs.aws.amazon.com/cli/latest/reference/'
+    index_url = '/index.html'
+    start_urls = []
+    for command in commands:
+        start_urls.append(base_url + str(command) + index_url)
+
+    def parse(self, response):
+        sel = Selector(response)
+        sites = sel.xpath('//div[@class="toctree-wrapper compound"]/ul/li')
+        items = []
+        for site in sites:
+            item = Website()
+            item['name'] = site.xpath('a/text()').extract()
+            items.append(item)
+        self.scraped_commands.extend(items)
+        return items
+
+    def handle_closed(self):
+        SOURCES_DIR = os.path.dirname(os.path.realpath(__file__))
+        SOURCES_PATH = os.path.join(SOURCES_DIR, 'data/AWSDOCS.txt')
+        with open(SOURCES_PATH, 'w') as f:
+            commands = self.commands
+            scraped_commands = []
+            for item in self.scraped_commands:
+                scraped_commands.append(str(item['name'][0]))
+            commands = sorted(list(set(commands)))
+            scraped_commands = sorted(list(set(scraped_commands)))
+            global_options = sorted(list(set(self.global_options)))
+            resource_options = sorted(list(set(self.resource_options)))
+            f.write('[commands]: ' + \
+                str(len(commands)) + '\n')
+            for item in commands:
+                f.write(item + '\n')
+            f.write('[sub_commands]: ' + \
+                str(len(scraped_commands)) + '\n')
+            for item in scraped_commands:
+                f.write(item + '\n')
+            f.write('[global_options]: ' + \
+                str(len(global_options)) + '\n')
+            for item in global_options:
+                f.write(item + '\n')
+            f.write('[resource_options]: ' + \
+                str(len(resource_options)) + '\n')
+            for item in resource_options:
+                f.write(item + '\n')
